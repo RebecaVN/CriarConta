5f2b3ca11817b13a9feefcebb0bf8cf5b1fb13aa
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 08:27:28 -0300

    Add files via upload

---
+++ CriarConta.java
@@ -0,0 +1,14 @@
+package Atividade3;
+
+public class CriarConta {
+	public static void main(String[] args) {
+		
+		Conta conta1 = new Conta(123, "Jerônimo Santos");
+		Conta conta2 = new Conta(123, "Lais Moreira");
+		Conta conta3 = new Conta(123, "Gilson Ribeiro");
+		
+		
+	
+	
+	}
+}
